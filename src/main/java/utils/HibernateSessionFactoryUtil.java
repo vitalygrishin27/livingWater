f275f527879820e2987321840e847ece0fd40d32
edevick <ekdevitskaya@gmail.com>
2019-03-27 02:54:10 +0200

    update added

---
+++ src/main/java/utils/HibernateSessionFactoryUtil.java
@@ -0,0 +1,27 @@
+package utils;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateSessionFactoryUtil {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+            sessionFactory = configuration.buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+
+    public static Session getSessionFactory() throws HibernateException {
+
+        return sessionFactory.openSession();
+
+    }
+}
